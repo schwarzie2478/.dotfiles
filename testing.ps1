ee8847dc0db566993f5ae93ab5d8e254471deb62
Stijn Peeters <schwarzie2478@hotmail.com>
2024-03-28 17:48:11 +0100

    $Backup config like  D previous.json
    Backup config on machine TONESPC\n\n Modified files:  \n D previous.json  D testing.ps1

--- testing.ps1
+++
@@ -1,8 +0,0 @@
-git show 396a:vscode/extensions.json > previous.json
-$previous = get-content .\previous.json | ConvertFrom-Json
-$current  = get-content .\vscode\extensions\extensions.json | ConvertFrom-Json
-
-$missingElements = Compare-Object $current $previous | Where-Object { $_.SideIndicator -eq '<=' } | Select-Object -ExpandProperty InputObject
-
-# Output the missing elements
-$missingElements
